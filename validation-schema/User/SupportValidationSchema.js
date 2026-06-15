8d72e1d38b9bc7ab9bfff9ba877193d5533d8a49
Mainak Saha <mainaksaha@Mainaks-MacBook-Air.local>
2026-06-15 17:32:07 +0530

    Update Support Send Validation Schema With Workspace ID

--- validation-schema/User/SupportValidationSchema.js
+++ validation-schema/User/SupportValidationSchema.js
@@ -5,6 +5,7 @@ module.exports.showSupportMessageSchema = Joi.object().keys({
 });
 
 module.exports.sendSupportMessageSchema = Joi.object().keys({
+    workspace_id: Joi.string().required(),
     message: Joi.string().trim().required(),
     importance: Joi.string().valid('normal', 'important', 'urgent').default('normal'),
 });
