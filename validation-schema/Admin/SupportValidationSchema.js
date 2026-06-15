ca198c09bdb458d4dcf3d3e97ec406fe127ed1c2
Mainak Saha <mainaksaha@Mainaks-MacBook-Air.local>
2026-06-15 18:15:26 +0530

    Update Admin Support Controller With Workspace ID

--- validation-schema/Admin/SupportValidationSchema.js
+++ validation-schema/Admin/SupportValidationSchema.js
@@ -1,11 +1,11 @@
 const Joi = require('joi');
 
 module.exports.supportThreadSchema = Joi.object().keys({
-    client_id: Joi.string().required(),
+    workspace_id: Joi.string().required(),
 });
 
 module.exports.sendSupportMessageSchema = Joi.object().keys({
-    client_id: Joi.string().required(),
+    workspace_id: Joi.string().required(),
     message: Joi.string().trim().required(),
     importance: Joi.string().valid('normal', 'important', 'urgent').default('normal'),
 });
